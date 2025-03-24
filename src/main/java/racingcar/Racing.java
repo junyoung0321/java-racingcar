fc4a7fbb7898356859c5ad82c7f48af0be92e1ca
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-03-24 22:03:06 +0900

    racing 클래스 우승자 출력 함수

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,21 +1,28 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.stream.Collectors;
+
 
 public class Racing {
-    private final List<car>  cars =  new ArrayList<>();
+    private final List<Car>  cars =  new ArrayList<>();
     private int rounds;
 
+
+    public void start() {
+        inputCars();
+        inputRounds();
+        play();
+        printWinners();
+    }
 
     private void inputCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] names = Console.readLine().split(",");
         for (String name : names) {
-            cars.add(new car(name.trim()));
+            cars.add(new Car(name.trim()));
         }
     }
 
@@ -45,5 +52,20 @@ public class Racing {
         System.out.println();
     }
 
+    private void printWinners() {
+        int maxPosition = 0;
+        List<String> winners = new ArrayList<>();
 
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+                winners.clear();
+                winners.add(car.getName());
+            } else if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        System.out.println("최종 우승자: " + String.join(", ", winners));
+    }
 }
