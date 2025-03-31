4c2ff9affd9cd7eaed81066c943a5e1a09983f77
junyoung0321 <dnfltkagudwp123@gmail.com>
2025-03-31 19:03:19 +0900

    이름 검증 메소드 분리

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -8,11 +8,15 @@ public class Car {
 
 
     public Car(String name) {
+        verificationName(name);
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void verificationName(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("차 이름은 5자 이하만 가능합니다.");
         }
-        this.name = name;
-        this.position = 0;
     }
 
     public void move() {
